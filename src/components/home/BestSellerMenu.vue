f0522aa4ae82c5f391eac2d07a6f6fe755d1f60c
cscthian <csantacruz1127@gmail.com>
2024-08-13 01:31:22 -0500

    pantalla home completa

--- src/components/home/BestSellerMenu.vue
+++ src/components/home/BestSellerMenu.vue
@@ -48,7 +48,7 @@
       class="hover:text-colorAction hover:cursor-pointer font-ffPrimary uppercase text-lg"
       @click="changeListBestProds('salud')">Bienestar</a>
   </div>
-  <div v-show="showProds" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
+  <div v-show="showProds" class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
     <ProductCardSkeleton 
       :visible="loading"
       :length="listProds.length"
